04157bc7c3ba1f4c3a574cdba73694f54d44f018
William Kristiawan <williamkristiawan38@gmail.com>
2023-09-15 17:14:17 +0700

    update

--- src/views/index-sections/SectionCarousel.js
+++ src/views/index-sections/SectionCarousel.js
@@ -1,63 +1,157 @@
+/*!
+
+=========================================================
+* Paper Kit React - v1.3.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
 import React from "react";
 import "../../assets/css/section.css";
-function SectionCarousel() {
-  return (
-<div id="carousel" className="carousel slide hero-slides" data-ride="carousel">
-  <ol className="carousel-indicators">
-    <li data-target="#carousel" data-slide-to={1} />
-    <li data-target="#carousel" data-slide-to={2} />
-  </ol>
-  <div className="carousel-inner" role="listbox">
-    <div className="carousel-item active boat">
-      <div className="container h-100 d-none d-md-block">
-        <div className="row align-items-center h-100">
-          <div className="col-12 col-md-9 col-lg-7 col-xl-6">
-            <div className="caption animated fadeIn">
-              <h2 className="animated fadeInLeft">Boat Excursions</h2>
-              <p className="animated fadeInRight">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tristique nisl vitae luctus sollicitudin. Fusce consectetur sem eget dui tristique, ac posuere arcu varius.</p>
-              <a className="animated fadeInUp btn delicious-btn" href="#">Learn more</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className="carousel-item sea">
-      <div className="container h-100 d-none d-md-block">
-        <div className="row align-items-center h-100">
-          <div className="col-12 col-md-9 col-lg-7 col-xl-6">
-            <div className="caption animated fadeIn">
-              <h2 className="animated fadeInLeft">Discover the canyon by the sea</h2>
-              <p className="animated fadeInRight">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tristique nisl vitae luctus sollicitudin. Fusce consectetur sem eget dui tristique, ac posuere arcu varius.</p>
-              <a className="animated fadeInUp btn delicious-btn" href="#">Learn more</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className="carousel-item river">
-      <div className="container h-100 d-none d-md-block">
-        <div className="row align-items-center h-100">
-          <div className="col-12 col-md-9 col-lg-7 col-xl-6">
-            <div className="caption animated fadeIn">
-              <h2 className="animated fadeInLeft">Explore the river valley</h2>
-              <p className="animated fadeInRight">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tristique nisl vitae luctus sollicitudin. Fusce consectetur sem eget dui tristique, ac posuere arcu varius.</p>
-              <a className="animated fadeInUp btn delicious-btn" href="#">Learn more</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <a className="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true" />
-    <span className="sr-only">Previous</span>
-  </a>
-  <a className="carousel-control-next" href="#carousel" role="button" data-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true" />
-    <span className="sr-only">Next</span>
-  </a>
-</div>
+// reactstrap components
+import {
+  Container,
+  Row,
+  Col,
+  Carousel,
+  CarouselItem,
+  CarouselIndicators,
+  CarouselCaption,
+} from "reactstrap";
 
+// core components
+
+const items = [
+  {
+    src: require("assets/img/padi.jpg"),
+    altText: "Founders",
+    caption: "Founders",
+  },
+  {
+    src: require("assets/img/corn.jpg"),
+    altText: "Founders",
+    caption: "Founders",
+  },
+  {
+    src: require("assets/img/singkong.jpg"),
+    altText: "Founders",
+    caption: "Founders",
+  },
+  {
+    src: require("assets/img/caliandraa.jpg"),
+    altText: "Founders",
+    caption: "Founders",
+  },
+];
+
+function SectionCarousel() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+  const [animating, setAnimating] = React.useState(false);
+  const onExiting = () => {
+    setAnimating(true);
+  };
+  const onExited = () => {
+    setAnimating(false);
+  };
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  };
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  };
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  };
+  return (
+    <>
+      <div className="carousels" id="carousel">
+        <Container>
+          <div className="ceo-title">
+            <h1>
+              KOMODITI <span>YANG DIBUDIDAYAKAN</span>
+            </h1>
+            <h4>PT Palma Pertiwi Makmur</h4>
+            <img src={require("../../assets/img/ceo-div.png")}></img>
+          </div>
+
+          <Row>
+            <Col className="page-carousel">
+              <Carousel
+                activeIndex={activeIndex}
+                next={next}
+                previous={previous}
+              >
+                <CarouselIndicators
+                  items={items}
+                  activeIndex={activeIndex}
+                  onClickHandler={goToIndex}
+                />
+                {items.map((item) => {
+                  return (
+                    <CarouselItem
+                      onExiting={onExiting}
+                      onExited={onExited}
+                      key={item.src}
+                    >
+                      <img
+                        className="image-carousel"
+                        src={item.src}
+                        alt={item.altText}
+                      />
+                      <CarouselCaption
+                        captionText={item.caption}
+                        captionHeader=""
+                      />
+                    </CarouselItem>
+                  );
+                })}
+                <a
+                  className="left carousel-control carousel-control-prev"
+                  data-slide="prev"
+                  href="#pablo"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    previous();
+                  }}
+                  role="button"
+                >
+                  <span className="fa fa-angle-left" />
+                  <span className="sr-only">Previous</span>
+                </a>
+                <a
+                  className="right carousel-control carousel-control-next"
+                  data-slide="next"
+                  href="#pablo"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    next();
+                  }}
+                  role="button"
+                >
+                  <span className="fa fa-angle-right" />
+                  <span className="sr-only">Next</span>
+                </a>
+              </Carousel>
+            </Col>
+          </Row>
+        </Container>
+      </div>{" "}
+    </>
   );
 }
 
